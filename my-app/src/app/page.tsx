392dc72b18d2492470ba630345e0edadffbad21e
Brian <118695080+B3YONDLIF3@users.noreply.github.com>
2023-10-19 16:30:11 -0400

    brian changes small nothing important

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
     <div className='px-6 py-54 sm:px-6 sm:py-32 lg:px-8'>
       <div className='mx-auto my-auto max-w-5xl text-center'>
         <h2 className='text-3xl font-bold tracking-tight text-white sm:text-7xl'>
-          Configurable. Persistent. Fast.
+          Fast. Persistent. Configurable.
         </h2>
         <p className='mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300'>
           Elevate your project with persistent caching and a fine-tuned Cache Invalidation Policy.

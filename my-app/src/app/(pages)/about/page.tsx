7075fd8f7ec4c3faf8f92ba9d3d2b6a46d737310
Jake Bayar <20175302+jakebayar@users.noreply.github.com>
2023-10-23 15:48:12 -0400

    about page is stable

--- my-app/src/app/(pages)/about/page.tsx
+++
@@ -1,4 +0,0 @@
-// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
-export default function Page() {
-    return <h1>Hello, About Page!</h1>;
-}
--- my-app/src/app/(pages)/features/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1>Hello, Features Page!</h1>;
-}

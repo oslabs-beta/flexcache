8358149669a27f670f00d86b034a86e361560b68
Jake Bayar <20175302+jakebayar@users.noreply.github.com>
2023-10-23 15:51:00 -0400

    latest

---
+++ my-app/src/app/(pages)/about/page.tsx
@@ -0,0 +1,11 @@
+import Team from '@/components/Team';
+
+
+export default function Page() {
+
+    return (
+        <main className='isolate'>
+            <Team />;
+        </main>
+    )
+}
---
+++ my-app/src/app/(pages)/demo/page.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import Search from '@/components/Search';
+
+export default function Page() {
+
+
+    const [data, setData] = useState<any>();
+
+    const handleClick = () => {
+        fetch('https://www.balldontlie.io/api/v1/players/237')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setData(data);
+                console.log(data);
+            })
+            .catch(error => {
+                console.log('There was a problem with the fetch operation:', error.message);
+            });
+    };
+
+
+    return (
+        <>
+            {/* <button
+                    type="button"
+                    onClick={() => handleClick()}
+                    className="flex items-center justify-center w-40 gap-2 px-3 py-2 text-sm font-regular text-white bg-green-600 border border-green-400 rounded-md shadow-lr hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500"
+                    style={{ boxShadow: '0 4px 4px rgba(0, 0, 0, .3)' }}
+                >
+                    <span style={{ textShadow: '0px 2px 4px rgba(0, 0, 0, .3)' }}>
+                        Search
+                    </span>
+                </button>
+                <div>
+                    {data && (
+                        <div>
+                            <p>Name: {data.first_name} {data.last_name}</p>
+                            <p>Height: {data.height_feet} feet {data.height_inches} inches</p>
+                        </div>
+                    )}
+                </div> */}
+            < Search />
+        </>
+        // < Search />
+    );
+}
